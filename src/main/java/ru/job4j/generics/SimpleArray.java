16febcefed625f49201a5f92bdc725ed517ef9d4
Michael Lobov <michael.lobov13@gmail.com>
2021-06-13 13:46:20 +0300

    5.2.1. Реализовать SimpleArray<T> [#156 #166316]

--- src/main/java/ru/job4j/generics/SimpleArray.java
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -4,64 +4,59 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class SimpleArray<T> implements Iterable<T> {
-    private T[] array;
-    private int length = 0;
+	private T[] array;
+	private int length = 0;
 
-    public SimpleArray(int size) {
-        this.array = (T[]) new Object[size];
-    }
+	public SimpleArray(int size) {
+		this.array = (T[]) new Object[size];
+	}
 
-    public void add(T model) {
-        array[length++] = model;
-    }
+	public void add(T model) {
+		array[length++] = model;
+	}
 
-    public T get(int index) {
-        T got = null;
-        if (checkIndex(index, this.length) == index) {
-            got = array[index];
-        }
-        return got;
-    }
+	public T get(int index) {
+		checkIndex(index, this.length);
+		return array[index];
+	}
 
-    public void set(int index, T model) {
-        if (checkIndex(index, length) == index) {
-            array[index] = model;
-        }
-    }
+	public void set(int index, T model) {
+		checkIndex(index, this.length);
+		array[index] = model;
+	}
 
-    public void remove(int index) {
-        if (checkIndex(index, length) == index) {
-            System.arraycopy(array, index + 1, array, index, length - index - 1);
-            array[length - 1] = null;
-            length--;
-        }
-    }
+	public void remove(int index) {
+		checkIndex(index, this.length);
+		System.arraycopy(array, index + 1, array, index, length - index - 1);
+		array[length - 1] = null;
+		length--;
+	}
 
-    public int checkIndex(int index, int length) {
-        if (0 <= index && index < length) {
-            return index;
-        } else {
-            throw new IndexOutOfBoundsException();
-        }
-    }
+	public int checkIndex(int index, int length) {
+		if (0 <= index && index < length) {
+			return index;
+		} else {
+			throw new IndexOutOfBoundsException();
+		}
+	}
 
-    @Override
-    public Iterator<T> iterator() {
-        return new Iterator<T>() {
-            int point = 0;
+	@Override
+	public Iterator<T> iterator() {
+		return new Iterator<T>() {
+			int point = 0;
 
-            @Override
-            public boolean hasNext() {
-                return point < length;
-            }
+			@Override
+			public boolean hasNext() {
+				return point < length;
+			}
 
-            @Override
-            public T next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException();
-                }
-                return array[point++];
-            }
-        };
-    }
+			@Override
+			public T next() {
+				if (!hasNext()) {
+					throw new NoSuchElementException();
+				}
+				return array[point++];
+			}
+		};
+	}
 }
