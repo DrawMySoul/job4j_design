23e5a3321286c6203fedb25d80fd6575419c894a
DrawMySoul <itismineeee@gmail.com>
2021-06-21 20:40:10 +0300

    2. Создать контейнер на базе связанного списка [#159 #169551]

--- src/main/java/ru/job4j/collection/list/SimpleLinkedList.java
+++ src/main/java/ru/job4j/collection/list/SimpleLinkedList.java
@@ -7,11 +7,6 @@ public class SimpleLinkedList<E> implements List<E> {
     private Node<E> lastNode;
     private int length = 0;
     private int modCount = 0;
-    private Node<E>[] container;
-
-    public SimpleLinkedList() {
-        container = new Node[5];
-    }
 
     private class Node<E> {
         private E item;
@@ -27,34 +22,34 @@ public class SimpleLinkedList<E> implements List<E> {
 
     @Override
     public void add(E value) {
-        checkLength();
-        if (length == 0) {
-            firstNode = new Node<>(value, null, lastNode);
-            lastNode = firstNode;
+        Node<E> newElement = new Node<>(value, lastNode, null);
+        if (firstNode == null) {
+            firstNode = newElement;
         } else {
-            lastNode = new Node<>(value, lastNode, null);
-            container[length - 1].next = lastNode;
+            lastNode.next = newElement;
         }
-        container[length++] = lastNode;
+        lastNode = newElement;
+        length++;
         modCount++;
     }
 
     @Override
     public E get(int index) {
         Objects.checkIndex(index, length);
-        return container[index].item;
-    }
-
-    public void checkLength() {
-        if (length == container.length) {
-            container = Arrays.copyOf(container, container.length * 2);
+        Node<E> foundNode = firstNode;
+        int count = 0;
+        while (count < index) {
+            foundNode = foundNode.next;
+            count++;
         }
+        return foundNode.item;
     }
 
     @Override
     public Iterator<E> iterator() {
         return new Iterator<E>() {
             private int expectedModCount = modCount;
+            private Node<E> element = firstNode;
             private int point = 0;
 
             @Override
@@ -62,7 +57,6 @@ public class SimpleLinkedList<E> implements List<E> {
                 if (expectedModCount != modCount) {
                     throw new ConcurrentModificationException();
                 }
-
                 return point < length;
             }
 
@@ -71,7 +65,10 @@ public class SimpleLinkedList<E> implements List<E> {
                 if (!hasNext()) {
                     throw new NoSuchElementException();
                 }
-                return container[point++].item;
+                E result = element.item;
+                element = element.next;
+                point++;
+                return result;
             }
         };
     }
