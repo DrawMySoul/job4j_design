f1a60a37cf2d084d5423eb940cce30eb0324fdc1
DrawMySoul <itismineeee@gmail.com>
2021-07-15 13:29:40 +0300

    0.1. FileOutputStream. [#252488 #177099]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -5,11 +5,14 @@ import java.io.FileOutputStream;
 public class ResultFile {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            StringBuilder result = new StringBuilder();
             for (int i = 1; i < 10; i++) {
                 for (int j = 1; j < 10; j++) {
-                    out.write(i * j);
+                    result.append(i*j + " ");
                 }
+                result.append("\r\n");
             }
+            out.write(result.toString().getBytes());
         } catch (Exception e) {
             e.printStackTrace();
         }
