29f0a38575b2d170e614e132dda67786b878d5f2
DrawMySoul <itismineeee@gmail.com>
2021-07-21 19:45:47 +0300

    1. Читаем файл конфигурации [#858 #178985]

--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.*;
 public class ConfigTest {
     @Test
     public void whenPairWithoutComment() {
-        String path = "pair_without_comment.properties";
+        String path = "./data/pair_without_comment.properties";
         Config config = new Config(path);
         config.load();
         assertThat(config.value("name"), is("Petr Arsentev"));
@@ -19,12 +19,12 @@ public class ConfigTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void whenPairWithViolation() {
-        new Config("pair_with_violation.properties").load();
+        new Config("./data/pair_with_violation.properties").load();
     }
 
     @Test
     public void whenPairWithComment() {
-        Config config = new Config("pair_with_comment.properties");
+        Config config = new Config("./data/pair_with_comment.properties");
         config.load();
         assertThat(config.value("#"), is(Matchers.nullValue()));
         assertThat(config.value(""), is(Matchers.nullValue()));
