d25be7ff526549c3d2b8da91b3d0f0727f0ddf2e
Michael Lobov <michael.lobov13@gmail.com>
2021-08-16 20:30:04 +0300

    7. Scanner [#504791 #190169]

--- src/main/java/ru/job4j/io/scanner/CSVReader.java
+++ src/main/java/ru/job4j/io/scanner/CSVReader.java
@@ -6,6 +6,39 @@ import java.io.*;
 import java.util.*;
 
 public class CSVReader {
+	private Map<Integer, List<String>> table = new HashMap<>();
+
+	public void readCSVFile(String path, String delimiter, String filter) {
+		try (Scanner scanner = new Scanner(new File(path))) {
+			while (scanner.hasNextLine()) {
+				String[] line = scanner.nextLine().split(delimiter);
+				if (table.isEmpty()) {
+					for (int i = 0; i < line.length; i++) {
+						if (filter.contains(line[i])) {
+							table.put(i, new ArrayList<>());
+						}
+					}
+				} else {
+					for (int i = 0; i < line.length; i++) {
+						if (table.containsKey(i)) {
+							table.get(i).add(line[i]);
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void write(String out) {
+		try (PrintStream printStream = out.equals("stdout") ? new PrintStream(System.out) : new PrintStream(new FileOutputStream(out))) {
+			table.values().forEach(printStream::println);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
 		if (args.length == 0) {
 			System.out.println("All arguments are absent. Use java -jar target/csvReader.jar -path=PATH -delimiter=DELIMITER_TYPE -out=OUT_FOLDER -filter=FILTER_TYPE");
@@ -16,56 +49,10 @@ public class CSVReader {
 		} else if (args.length == 3) {
 			System.out.println("Argument filter is absent. Use java -jar target/csvReader.jar -path=PATH -delimiter=DELIMITER_TYPE -out=OUT_FOLDER -filter=FILTER_TYPE");
 		}
-		CSVReader csvReader = new CSVReader();
+
 		ArgsName argsName = ArgsName.of(args);
-		Map<String, List<String>> map = csvReader.readCSVFile(argsName.get("path"), argsName.get("delimiter"));
-
-		if (argsName.get("out").equals("stdout")) {
-			csvReader.stdout(argsName.get("filter"), map);
-		} else {
-			csvReader.writeToFile(map, argsName.get("out"), argsName.get("filter"));
-		}
-	}
-
-	public Map<String, List<String>> readCSVFile(String path, String delimiter) {
-		Map<String, List<String>> table = new LinkedHashMap<>();
-		try (Scanner scanner = new Scanner(new File(path))) {
-			while (scanner.hasNextLine()) {
-				String[] some = scanner.nextLine().split(delimiter);
-				if (table.isEmpty()) {
-					for (String s : some) {
-						table.put(s, new ArrayList<>());
-					}
-				} else {
-					int count = 0;
-					for (Map.Entry<String, List<String>> m : table.entrySet()) {
-						m.getValue().add(some[count++]);
-					}
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return table;
-	}
-
-	public void stdout(String filter, Map<String, List<String>> table) {
-		for (Map.Entry<String, List<String>> column : table.entrySet()) {
-			if (filter.contains(column.getKey())) {
-				System.out.println(column.getValue());
-			}
-		}
-	}
-
-	public void writeToFile(Map<String, List<String>> table, String outPath, String filter) {
-		try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(outPath))) {
-			for (Map.Entry<String, List<String>> column : table.entrySet()) {
-				if (filter.contains(column.getKey())) {
-					out.write(column.getValue().toString().getBytes());
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		CSVReader csvReader = new CSVReader();
+		csvReader.readCSVFile(argsName.get("path"), argsName.get("delimiter"), argsName.get("filter"));
+		csvReader.write(argsName.get("out"));
 	}
 }
