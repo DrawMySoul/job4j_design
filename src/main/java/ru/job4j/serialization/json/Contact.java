b529ae03d88c21c995b0aba038d34819653d94ba
Michael Lobov <michael.lobov13@gmail.com>
2021-09-26 19:21:32 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064] lesson

--- src/main/java/ru/job4j/serialization/json/Contact.java
+++ src/main/java/ru/job4j/serialization/json/Contact.java
@@ -2,12 +2,21 @@ package ru.job4j.serialization.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Contact {
 	private final String phone;
 
 	public Contact(String phone) {
 		this.phone = phone;
+	}
+
+	public String getPhone() {
+		return phone;
 	}
 
 	@Override
@@ -18,13 +27,13 @@ public class Contact {
 	}
 
 	public static void main(String[] args) {
-		final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
+		/*final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
 
-		/* Преобразуем объект person в json-строку. */
+		*//* Преобразуем объект person в json-строку. *//*
 		final Gson gson = new GsonBuilder().create();
 		System.out.println(gson.toJson(person));
 
-		/* Модифицируем json-строку */
+		*//* Модифицируем json-строку *//*
 		final String personJson =
 			"{"
 				+ "\"sex\":false,"
@@ -37,7 +46,30 @@ public class Contact {
 				+ "[\"Student\",\"Free\"]"
 				+ "}";
 		final Person personMod = gson.fromJson(personJson, Person.class);
-		System.out.println(personMod);
+		System.out.println(personMod);*/
+
+		/* JSONObject из json-строки строки */
+		JSONObject jsonContact = new JSONObject("{\"phone\":\"+7(924)111-111-11-11\"}");
+
+		/* JSONArray из ArrayList */
+		List<String> list = new ArrayList<>();
+		list.add("Student");
+		list.add("Free");
+		JSONArray jsonStatuses = new JSONArray(list);
+
+		/* JSONObject напрямую методом put */
+		final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("sex", person.isSex());
+		jsonObject.put("age", person.getAge());
+		jsonObject.put("contact", jsonContact);
+		jsonObject.put("statuses", jsonStatuses);
+
+		/* Выведем результат в консоль */
+		System.out.println(jsonObject.toString());
+
+		/* Преобразуем объект person в json-строку */
+		System.out.println(new JSONObject(person).toString());
 	}
 
 }
