24e672222c44d8f0f6112ae656bc2c76552fe453
Michael Lobov <michael.lobov13@gmail.com>
2022-04-11 20:29:41 +0300

    1. Inner join [#373617 #287238]

--- JDBC/inner_join.sql
+++ JDBC/inner_join.sql
@@ -13,15 +13,15 @@ CREATE TABLE film(
 INSERT INTO director(full_name) VALUES ('Coen brothers');
 INSERT INTO director(full_name) VALUES ('Paul Thomas Anderson');
 
-INSERT INTO film(director_id, title, release) 
+INSERT INTO film(director_id, title, release_year) 
 VALUES (1, 'No Country For Old Men', 2007);
-INSERT INTO film(director_id, title, release) 
+INSERT INTO film(director_id, title, release_year) 
 VALUES (1, 'Inside Llewyn Davis', 2013);
-INSERT INTO film(director_id, title, release) 
+INSERT INTO film(director_id, title, release_year) 
 VALUES (2, 'There Will Be Blood', 2007);
-INSERT INTO film(director_id, title, release) 
+INSERT INTO film(director_id, title, release_year) 
 VALUES (2, 'The Master', 2012);
-INSERT INTO film(director_id, title, release) 
+INSERT INTO film(director_id, title, release_year) 
 VALUES ('Primer', 2004);
 
 SELECT d.full_name AS director, f.title, f.release_year AS year FROM film AS f
