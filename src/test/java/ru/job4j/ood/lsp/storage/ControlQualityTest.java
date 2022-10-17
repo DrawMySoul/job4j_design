5c59fc4e4f87e39a3c247b919d572734c78447f7
Michael Lobov <michael.lobov13@gmail.com>
2022-10-17 23:41:40 +0300

    2. Парковка машин [#853] fix tests in ControlQualityTest

--- src/test/java/ru/job4j/ood/lsp/storage/ControlQualityTest.java
+++ src/test/java/ru/job4j/ood/lsp/storage/ControlQualityTest.java
@@ -18,23 +18,23 @@ class ControlQualityTest {
         ControlQuality controller = new ControlQuality(List.of(shop, warehouse, trash));
         List<Food> foods = List.of(
             new Food("bread",
-                LocalDate.of(2022, Month.OCTOBER, 12),
-                LocalDate.of(2022, Month.OCTOBER, 29),
+                LocalDate.now().minusDays(4),
+                LocalDate.now().plusDays(13),
                 10
             ),
             new Food("meat",
-                LocalDate.of(2022, Month.OCTOBER, 12),
-                LocalDate.of(2022, Month.OCTOBER, 20),
+                LocalDate.now().minusDays(4),
+                LocalDate.now().plusDays(4),
                 500
             ),
             new Food("chicken",
-                LocalDate.of(2022, Month.OCTOBER, 12),
-                LocalDate.of(2022, Month.OCTOBER, 17),
+                LocalDate.now().minusDays(4),
+                LocalDate.now().plusDays(1),
                 100
             ),
             new Food("banana",
-                LocalDate.of(2022, Month.OCTOBER, 12),
-                LocalDate.of(2022, Month.OCTOBER, 12),
+                LocalDate.now().minusDays(4),
+                LocalDate.now().minusDays(4),
                 60
             )
         );
@@ -61,8 +61,8 @@ class ControlQualityTest {
         ControlQuality controller = new ControlQuality(List.of(shop, warehouse, trash));
         int price = 1000;
         Food food = new Food("food",
-            LocalDate.of(2022, Month.OCTOBER, 12),
-            LocalDate.of(2022, Month.OCTOBER, 17),
+            LocalDate.now().minusDays(4),
+            LocalDate.now().plusDays(1),
             price);
         int expected = (int) (price * food.getDiscount());
         controller.control(food);
