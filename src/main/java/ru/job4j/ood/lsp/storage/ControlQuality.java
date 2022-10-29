34882f9cf86ee1a64b4f08f7497020e8bee0e783
Michael Lobov <michael.lobov13@gmail.com>
2022-10-29 17:28:31 +0300

    1. Динамическое перераспределение продуктов [#854]

--- src/main/java/ru/job4j/ood/lsp/storage/ControlQuality.java
+++ src/main/java/ru/job4j/ood/lsp/storage/ControlQuality.java
@@ -1,5 +1,6 @@
 package ru.job4j.ood.lsp.storage;
 
+import java.util.Collection;
 import java.util.List;
 
 public class ControlQuality {
@@ -11,5 +12,12 @@ public class ControlQuality {
 
     public void control(Food food) {
         stores.forEach(s -> s.add(food));
+    }
+
+    public void resort() {
+        stores.stream()
+            .map(Store::getFoods)
+            .flatMap(Collection::stream)
+            .forEach(this::control);
     }
 }
